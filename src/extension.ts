b3372648a818dc62541070c44ccf3efe7862547e
FahadZaheerfzr <fahadzaheer19@gmail.com>
2022-03-20 03:36:40 +0500

    Added side panel

--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,34 @@
 const dotenv = require('dotenv');
 dotenv.config({path:'D:/code/extensions/vs code/devrev/autoflow/.env'});
 
+import { NodeDependenciesProvider } from './TreeView/nodeDependencies';
 import * as vscode from 'vscode';
 import * as API from './API';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
+	const rootPath =
+	vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+	  ? vscode.workspace.workspaceFolders[0].uri.fsPath
+	  : "";
+
+	  
+  const nodeDependenciesProvider = new NodeDependenciesProvider(rootPath);
+	  
+  vscode.window.registerTreeDataProvider(
+	'nodeDependencies',
+	new NodeDependenciesProvider(rootPath)
+  );
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "autoflow" is now active!');
-
+	vscode.window.createTreeView('nodeDependencies', {
+		treeDataProvider: new NodeDependenciesProvider(rootPath)
+	  });
+	  vscode.commands.registerCommand('nodeDependencies.refreshEntry', () =>
+    nodeDependenciesProvider.refresh()
+  );
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
